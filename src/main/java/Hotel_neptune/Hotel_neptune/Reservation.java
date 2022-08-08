859da29523fba81a06e2be1cf6b960e9d100b0b8
Kevin Carbeti <kevincarbeti@gmail.com>
2022-08-08 10:23:29 +0200

    ajout fichiers FXML

---
+++ src/main/java/Hotel_neptune/Hotel_neptune/Reservation.java
@@ -0,0 +1,13 @@
+package Hotel_neptune.Hotel_neptune;
+
+import java.util.Date;
+
+public class Reservation {
+
+	private Chambre room;
+	private Date debut;
+	private Date fin;
+	private String nomClient;
+	
+	
+}
--- src/main/java/Hotel_neptune/Hotel_neptune/Test.java
+++ src/main/java/Hotel_neptune/Hotel_neptune/Test.java
@@ -1,6 +1,10 @@
 package Hotel_neptune.Hotel_neptune;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -9,13 +13,27 @@ import javafx.stage.Stage;
 public class Test extends Application {
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage){
+//        String javaVersion = System.getProperty("java.version");
+//        String javafxVersion = System.getProperty("javafx.version");
+//        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+//        Scene scene = new Scene(new StackPane(l), 640, 480);
+//        stage.setScene(scene);
+//        stage.show();
+    	Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("FXML/SignIn.fxml"));
+			stage.setTitle("Hello World");  
+
+	        Scene scene = new Scene(root, 900, 900);  
+
+	        stage.setScene(scene);  
+	        stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+        
     }
 
     public static void main(String[] args) {
